bdd2e101dc8e38335e22abbaae7929a562bde4dd
tqa309 <tranquocanh.3009@gmail.com>
2021-08-14 20:21:07 +0700

    change hello

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello, World!');
+	res.send('Hello, everyone!');
 })
 
 app.listen(port, () => console.log(`Example app running on port: ${port}`));
